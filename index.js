48adc90f8935d8b0a507395c357796bb22d542cc
globalitsources <globalitsourcesreact@gmail.com>
2025-07-21 20:11:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "https://erp-gits-baea7f60.vercel.app/"
+     "https://erp-gits-baea7f60.vercel.app/"
 
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
